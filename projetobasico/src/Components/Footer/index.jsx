314e0d67baaddae8de67b0869903ff0f77b9828a
maluzete1864 <maria.meira2@estudante.ifms.edu.br>
2025-10-16 15:12:30 -0400

    aula 16/10/25

---
+++ projetobasico/src/Components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer () {
+    return (
+        <footer className={styles.footer}>
+            <p>Feito com carinho para você. Volte sempre!!</p>
+            
+        </footer>
+    );
+}
+ export default Footer;
